e1e5709bcb70ad4717ae32ff5322f5f70b5cca16
sgchoo <choo19950306@gmail.com>
2024-06-23 18:40:54 +0900

    err handling code add

--- src/User/login/login.controller.ts
+++ src/User/login/login.controller.ts
@@ -4,10 +4,16 @@ import { userLogin } from './login.service';
 
 const router = express.Router();
 
-router.post("/login", async (req: Request, res: Response): Promise<LoginOutput> => {
-    const info: LoginInfo = req.body as LoginInfo;
-
-    return await userLogin(info);
+router.post("/login", async (req: Request, res: Response, next: NextFunction): Promise<LoginOutput | null> => {
+    try {
+        const info: LoginInfo = req.body as LoginInfo;
+    
+        return await userLogin(info);
+    }
+    catch(err) {
+        next(err);
+        return null;
+    }
 })
 
 export default router;
